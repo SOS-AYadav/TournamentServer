35485d39ad3b4bf3a3e86d8233475533c0d0a544
root <root@ip-172-16-0-144.ec2.internal>
2022-08-14 19:06:16 +0000

    socket changes

--- src/controllers/socketApi.js
+++ src/controllers/socketApi.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
+const { PlayersModel } = require('../models/players');
 const { getRoomId } = require('./getRoomId');
 const { usersMove } = require('./usersMove');
+
+let users = {};
 
 const socketApi = (io) => {
     io.use((socket, next) => {
@@ -42,7 +45,34 @@ const socketApi = (io) => {
                 )
             );
         }
-    }).on('connection', (socket) => {
+    }).on('connection', async (socket) => {
+        let roomId = '';
+        if (socket.user) {
+            try {
+                const player = await PlayersModel.findOne({
+                    username: socket.user.username,
+                }).populate('room');
+                roomId = player.room[player.room.length - 1].id;
+                if (users[roomId]) {
+                    users[roomId].add(socket.user.username);
+                    for (const user of users[roomId]) {
+                        console.log('uzzzz', user);
+                        io.to(roomId).emit('active', {
+                            status: 'ok',
+                            data: {
+                                username: user,
+                                active: true,
+                            },
+                            error: '',
+                        });
+                    }
+                } else {
+                    users[roomId] = new Set();
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
         socket.on('usermove', (msg) => {
             socket.body = msg;
             usersMove(socket);
@@ -51,32 +81,55 @@ const socketApi = (io) => {
             getRoomId(socket);
         });
 
-        socket.on('online', (roomId) => {
-            console.log('onlinee', socket.rooms);
-            io.to(roomId).emit('active', {
-                status: 'ok',
-                data: {
-                    username: socket.user.username,
-                    active: true,
-                },
-                error: '',
-            });
-        });
+        // socket.on('online', (roomId) => {
+        //     console.log('onlinee', socket.rooms);
+
+        //     console.log(users);
+        //     io.to(roomId).emit('active', {
+        //         status: 'ok',
+        //         data: {
+        //             username: socket.user.username,
+        //             active: true,
+        //         },
+        //         error: '',
+        //     });
+        // });
 
         socket.on('audience', (data) => {
             socket.join(data.roomId);
             console.log(socket.rooms, data);
         });
 
-        socket.on('disconnecting', (roomId) => {
-            socket.to(roomId).emit('active', {
-                status: 'ok',
-                data: {
-                    username: socket.user.username,
-                    active: false,
-                },
-                error: '',
-            });
+        socket.on('disconnecting', async () => {
+            if (socket.user) {
+                try {
+                    users[roomId].delete(socket.user.username);
+                    io.to(roomId).emit('active', {
+                        status: 'ok',
+                        data: {
+                            username: socket.user.username,
+                            active: false,
+                        },
+                        error: '',
+                    });
+                    if (users[roomId].size === 0) {
+                        delete users[roomId];
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+            console.log('disconnecttingg....', socket.user.username);
+            // if (socket.user.username) {
+            //     users = users[socket.user.username]?.filter(
+            //         (id) => id === socket.id
+            //     );
+            //     if (users?.[socket.user.username]?.length === 0) {
+
+            // delete users[socket.user.username];
+            // }
+            // }
         });
     });
 };
